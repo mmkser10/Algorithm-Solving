90c42ad8ade95e0c233fb07d340c4241ecaf66ee
Oflo <57768558+mmkser10@users.noreply.github.com>
2020-09-14 12:39:01 +0900

    [WHILE] Alogrithm solving clear

---
+++ 02439/src/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+
+        for(int i = A ; i >= 1 ; i--){
+            for(int y = i ; y <= A ; y++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
